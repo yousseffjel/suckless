31eff4179d39130db2ab911eeb2993f392100818
yousseffjel <youssef.fjel-etu@etu.univh2c.ma>
2023-10-09 17:54:58 +0100

    Initial commit

---
+++ dmenu/Material_Dark.h
@@ -0,0 +1,10 @@
+/* static const unsigned int bgalpha = 0xc8; */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm]            =   { "#e4e4e4", "#212121"},
+	[SchemeSel]             =   { "#550087", "#9a9a9a" },
+	[SchemeSelHighlight]    =   { "#134eb2", "#a94dbb" },
+	[SchemeNormHighlight]   =   { "#b7141e", "#212121" },
+	[SchemeOut]             =   { "#fee92e", "#7aba39" },
+	[SchemeHp]              =   { "#b7141e", "#424242" }
+};
---
+++ dwm/patch/include.h
@@ -0,0 +1,27 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+#include "bar.h"
+
+#include "bar_alpha.h"
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_status2d.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+#include "bar_systray.h"
+
+/* Other patches */
+#include "attachx.h"
+#include "autostart.h"
+#include "cyclelayouts.h"
+#include "floatpos.h"
+#include "movestack.h"
+#include "pertag.h"
+#include "rotatestack.h"
+#include "scratchpad.h"
+#include "vanitygaps.h"
+/* Layouts */
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
